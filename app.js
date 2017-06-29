2e3c3a9906d3d1c47f9a6dd238f59ee857b9f01c
Etienne FICHOT <efichot@e3r4p1.42.fr>
2017-06-29 13:27:13 +0200

    cookie-parser and body-parser set up

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 // const express = require('express');
 import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+
+// need to tell express to use middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render('index');
+	res.render('index');
 });
 
 app.get('/cards', (req, res) => {
-    res.render('card', { prompt: 'Who is buried in Grants tomb?', hint: 'Think about whose tomb it is.' });
+	res.render('card', { prompt: 'Who is buried in Grants tomb?', hint: 'Think about whose tomb it is.' });
+});
+
+app.get('/hello', (req, res) => {
+	res.render('hello', { name: req.cookies.username });
+});
+
+app.post('/hello', (req, res) => {
+	res.cookie('username', req.body.username);
+	res.render('hello', { name: req.body.username });
 });
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000');
+	console.log('Listening on port 3000');
 });
